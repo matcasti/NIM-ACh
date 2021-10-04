3a86a6738c196add102229c4f6bd0dc88bec1eb8
matcasti <matcasti@umag.cl>
2021-10-04 13:37:18 -0300

    NIM-ACh v2.0

---
+++ news_helper.R
@@ -0,0 +1,54 @@
+
+# News builder helper -------------------------------------------------------------------------
+
+news_generator <- function(news, for_sidebar = TRUE) {
+
+  sidebar <- '
+  <article>
+  	<a href="{root}news/news-{news$current}.html" class="image"><img src="{root}images/{news$img}" alt="" /></a>
+  	<p>{news$main}</p>
+  </article>
+  '
+  index_or_news <- '
+  <article>
+  	<a href="{root}news/news-{news$current}.html" class="image"><img src="{root}images/{news$img}" alt="" /></a>
+  	<h3>{news$main}</h3>
+  	<p>{news$desc}</p>
+  	<ul class="actions">
+  		<li><a href="{root}news/news-{news$current}.html" class="button">Más</a></li>
+  	</ul>
+  </article>
+  '
+  if (isTRUE(for_sidebar)) {
+    glue::glue(sidebar)
+  } else {
+    glue::glue(index_or_news)
+  }
+}
+
+news <- list(
+  ## Post number 2 ----
+  list(
+    current = 2,
+    # Title of post
+    main = "NIM-ACh formará parte de los evaluadores del 1er Concurso de Innovación, Ciencia y Tecnología de HABITAT",
+    # Description of article
+    desc = "Este proyecto es desarrollado por la Universidad Arturo Prat de Chile y cuenta con la colaboración de destacados profesores en el campo de la investigación del clima extremo en el país.", 
+    # Only name of file (with file extension)
+    img = "news-2-fig-1.png"
+  )
+  
+  ## Post number 1 ----
+  , list(
+    current = 1,
+    # Title of post
+    main = "Hemos vuelto a crear fuertes alianzas con el proyecto MEDIANTAR de la Universidad de Minas Gerais, Brasil",
+    # Description of article
+    desc = "Se establecen acuerdos y colaboración para la campaña Antártica 2021-2022 entre Brasil, Perú y Chile.", 
+    # Main picture for post (path to file)
+    img = "news-1-fig-1.png"
+  )
+)
+
+# Number of current posts
+current <- seq_len(length.out = length(news))
---
+++ site_generator.R
@@ -0,0 +1,108 @@
+
+# Prepare workspace ---------------------------------------------------------------------------
+
+
+# Load required libraries
+library(glue)
+
+# And source helper scritps
+source("news_helper.R")
+
+# Remove directory if exists (to start over)
+unlink(x = "_site", recursive = TRUE)
+
+
+# Create folders with subdirectories ----------------------------------------------------------
+
+
+# Create directory
+dir.create("_site")
+dir.create("_site/news")
+
+# Copy the content 'i' in root to _site
+for (i in c("assets", "images")) {
+  dir.create(paste0("_site/", i))
+  file.copy(
+    from = list.files(i, full.names = TRUE),
+    to = paste0("_site/", i),
+    recursive = TRUE
+  )
+}
+
+# Main pages ----------------------------------------------------------------------------------
+# Index, News and Research
+
+# Root (for sidebar in nested directories)
+root <- ""
+
+# Create news for sidebar
+news_sidebar <- lapply(head(news, n = 3), news_generator, TRUE) |>
+  paste0(collapse = "\n\n")
+
+# Create news for index and news page
+news_index <- lapply(head(news), news_generator, FALSE) |>
+  paste0(collapse = "\n\n")
+  
+# Create sidebar
+sidebar <- readLines("resources/sidebar.txt") |>
+  paste0(collapse = "\n") |>
+  glue::glue()
+
+social_media <- readLines("resources/social_media.txt") |>
+  paste0(collapse = "\n")
+
+# Create HOMEPAGE
+readLines("resources/index.txt") |>
+  paste0(collapse = "\n") |>
+  glue::glue() |>
+  cat(file = "_site/index.html")
+
+# Create NEWS page
+readLines("resources/news.txt") |>
+  paste0(collapse = "\n") |>
+  glue::glue() |>
+  cat(file = "_site/news.html")
+
+# Create RESEARCH page
+readLines("resources/research.txt") |>
+  paste0(collapse = "\n") |>
+  glue::glue() |>
+  cat(file = "_site/research.html")
+
+# Create MEMBERS page
+readLines("resources/members.txt") |>
+  paste0(collapse = "\n") |>
+  glue::glue() |>
+  cat(file = "_site/members.html")
+
+# Create ABOUT page
+readLines("resources/about.txt") |>
+  paste0(collapse = "\n") |>
+  glue::glue() |>
+  cat(file = "_site/about.html")
+
+# News posts ----------------------------------------------------------------------------------
+
+root <- "../"
+
+# Create news for sidebar within news directory
+news_sidebar <- lapply(head(news, n = 3), news_generator, TRUE) |>
+  paste0(collapse = "\n\n")
+
+# Create new nested sidebar
+sidebar_nested <- readLines("resources/sidebar.txt") |>
+  paste0(collapse = "\n") |>
+  glue::glue()
+
+# Create news
+for (i in current) {
+  body <- readLines(paste0("news/news-", i, ".txt")) |>
+    paste0(collapse = "\n")
+  
+  readLines("resources/news-template.txt") |> 
+    paste0(collapse = "\n") |>
+    glue::glue() |>
+    cat(file = paste0("_site/news/news-", i, ".html"))
+}
+
+
